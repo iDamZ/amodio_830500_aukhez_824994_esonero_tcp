15953dff227538a06af74ac580b6cae9acc013f1
iDamZ <amodiodamiano99@gmail.com>
2025-11-22 11:45:05 +0100

    Completato il main client
    Bisogna testare

--- client-project/src/main.c
+++ client-project/src/main.c
@@ -28,6 +28,21 @@
 
 #define NO_ERROR 0
 
+/* --- DEFINIZIONE STRUTTURE (PROTOCOLLO) --- */
+
+// Messaggio di Richiesta (Client -> Server)
+typedef struct {
+    char type;        // 't', 'h', 'w', 'p'
+    char city[64];    // Nome città (stringa null-terminated)
+} richiesta_client;
+
+// Messaggio di Risposta (Server -> Client)
+typedef struct {
+    unsigned int status;  // 0=Ok, 1=No City, 2=Invalid
+    char type;            // Eco del tipo
+    float value;          // Valore numerico
+} risposta_server;
+
 void clearwinsock() {
 #if defined WIN32
 	WSACleanup();
@@ -37,6 +52,42 @@ void clearwinsock() {
 int main(int argc, char *argv[]) {
 
 	// TODO: Implement client logic
+
+	char *server_ip = "127.0.0.1";
+	   int port = 56700;
+	char* input_string = NULL; // Stringa da inviare
+
+	for (int i = 1; i < argc; i++) {
+	        if (strcmp(argv[i], "-s") == 0 && (i + 1) < argc) {
+	            server_ip = argv[++i];
+	        } else if (strcmp(argv[i], "-p") == 0 && (i + 1) < argc) {
+	            port = atoi(argv[++i]);
+	        } else if (strcmp(argv[i], "-r") == 0 && (i + 1) < argc) {
+	            input_string = argv[++i];
+	        }
+	    }
+
+	    // Controllo obbligatorio: se manca -r, terminiamo
+	    if (input_string == NULL) {
+	        fprintf(stderr, "Errore: parametro -r obbligatorio\n");
+	        return -1;
+	    }
+
+	        // Esempio: "t bari" -> type='t', city="bari"
+	        richiesta_client req;
+	        memset(&req, 0, sizeof(req));
+	        req.type = input_string[0];
+	        if (strlen(input_string) > 1) {
+	            char *city_start = input_string + 1;
+	            // Se c'è uno spazio subito dopo il tipo, lo saltiamo
+	            if (*city_start == ' ') {
+	                city_start++;
+	            }
+	            strncpy(req.city, city_start, 63);
+	        } else {
+	            req.city[0] = '\0';
+	        }
+
 
 #if defined WIN32
 	// Initialize Winsock
@@ -65,8 +116,8 @@ int main(int argc, char *argv[]) {
 	struct sockaddr_in sad;
 	memset(&sad, 0, sizeof(sad));
 	sad.sin_family = AF_INET;
-	sad.sin_addr.s_addr = inet_addr( "127.0.0.1" );
-	sad.sin_port = htons( 56700 );
+	sad.sin_addr.s_addr = inet_addr(server_ip);
+	sad.sin_port = htons(port);
 
 	// ...
 
@@ -86,33 +137,56 @@ int main(int argc, char *argv[]) {
 	// send(...);
 	// recv(...);
 
-	char* input_string = "prova"; // Stringa da inviare
-	int string_len = strlen(input_string); // Determina la lunghezza
-	if (send(my_socket, input_string, string_len, 0) != string_len) {
-	perror("send() sent a different number of bytes than expected");
-	closesocket(my_socket);
-	clearwinsock();
-	return -1;
-	}
+	if (send(my_socket, &req, sizeof(req), 0) < 0) {
+	        perror("Send failed");
+	        close(my_socket);
+	        return -1;
+	    }
+
 
 
 	int bytes_rcvd;
 	int total_bytes_rcvd = 0;
-	char buf[BUFFERSIZE]; // buffer for data from the server
-	memset(buf, 0, BUFFERSIZE); // ensures extra bytes contain 0
 	printf("Received: "); // Setup to print the echoed string
-	while (total_bytes_rcvd < string_len) {
-	if ((bytes_rcvd = recv(my_socket, buf, BUFFERSIZE - 1, 0)) <= 0)
+	risposta_server resp;
+	while (total_bytes_rcvd < sizeof(req)) {
+	if ((bytes_rcvd = recv(my_socket, &resp, sizeof(resp), 0)) <= 0)
 	{
-	perror("recv() failed or connection closed prematurely");
+	perror("Receive failed");
 	closesocket(my_socket);
 	clearwinsock();
 	return -1;
 	}
 	total_bytes_rcvd += bytes_rcvd; // Keep tally of total bytes
 	}
-	buf[bytes_rcvd] = '\0'; // ridondante se si fa memset prima
-	printf("%s", buf); // Print the echo buffer
+
+	if (resp.status == 0) {
+		printf("Ricevuto risultato dal server ip %s. ", server_ip);
+	        switch (req.type) {
+	            case 't':
+	                printf("%s: Temperatura = %.1f°C\n", req.city, resp.value);
+	                break;
+	            case 'h':
+	                printf("%s: Umidità = %.1f%%\n", req.city, resp.value);
+	                break;
+	            case 'w':
+	                printf("%s: Vento = %.1f km/h\n", req.city, resp.value);
+	                break;
+	            case 'p':
+	                printf("%s: Pressione = %.1f hPa\n", req.city, resp.value);
+	                break;
+	            default:
+	                // Caso in cui il server risponde successo ma il tipo non è riconosciuto dal client
+	                printf("Tipo sconosciuto\n");
+	                break;
+	        }
+	    } else if (resp.status == 1) {
+	        // ERRORE: Città non disponibile
+	        printf("Città non disponibile\n");
+	    } else {
+	        // ERRORE: Richiesta non valida (status 2 o altro)
+	        printf("Richiesta non valida\n");
+	    }
 
 	// TODO: Close socket
 	// closesocket(my_socket);
